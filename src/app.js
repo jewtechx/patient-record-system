9ff1984b4e9d01c2bb5acd4bb426631f139b0535
jewtechx <jklarbi_danquah@st.ug.edu.gh>
2023-12-18 10:13:38 +0000

    patient vitals controllers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const patientRoute = require("./routes/patient.route")
+const visitationRoute = require("./routes/visitation.route")
 
 const app = express()
 
 app.use("/patients",patientRoute)
+app.use("/visitation",visitationRoute)
 
 module.exports = app
---
+++ src/controllers/pvitals.controller.js
@@ -0,0 +1,25 @@
+
+
+async function HTTPGetPVitals(req,res){
+    const visitation = await visitation_model.find()
+    res.status(200).json(visitation)
+}
+
+async function HTTPCreateVisitations(req,res){
+    const {patient_id,data_and_time,type_of_encounter
+    } = req.body
+
+    const pvitals = new ({
+        patient_id,data_and_time,type_of_encounter
+    })
+
+    if(patient){
+        res.status(200).json({message:"Patient vitals created successfully"})
+    }else{
+        res.status(500).json({error:"Internal server error"})
+    }
+}
+
+module.exports = {
+    HTTPGetVisitations,HTTPCreateVisitations
+}
